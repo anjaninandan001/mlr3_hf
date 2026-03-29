8f67f4420dbfd3dcb94bce03c9f06d27d1427785
Anjani Nandan <145668933+anjaninandan001@users.noreply.github.com>
2026-03-29 21:13:43 +0530

    Update test-htsk.R

--- tests/testthat/test-htsk.R
+++ tests/testthat/test-htsk.R
@@ -1,15 +1,1 @@
-test_that("htsk creates TaskClassif", {
-  task <- htsk(
-    repo_id = "scikit-learn/iris",
-    filename = "Iris.csv",
-    target = "Species"
-  )
-  expect_s3_class(task, "TaskClassif")
-})
-
-test_that("error on wrong target", {
-  expect_error(
-    htsk("scikit-learn/iris", "Iris.csv", "WrongColumn")
-  )
-})
 
